274e949b93fde3fb7ce0222b45e09293d850cce9
sivasamyd <88651955+sivasamyd@users.noreply.github.com>
2021-09-21 14:22:04 +0530

    Update AreaOfTriangle.java

--- Assignment_1/AreaOfTriangle.java
+++ Assignment_1/AreaOfTriangle.java
@@ -1,19 +1,13 @@
 package program1;
 import java.util.Scanner;
 public class AreaOfTriangle {
-
 	   public static void main(String args[]) 
 	    {   
-	       
 	      Scanner s= new Scanner(System.in);
-	        
-	         System.out.println("Enter the Base of the Triangle:");
-	         double b= s.nextDouble();
-	 
-	         System.out.println("Enter the height of the Triangle:");
-	          double h= s.nextDouble();
-	 
-	                  //Area = (width*height)/222222
+	      System.out.println("Enter the Base of the Triangle:");
+	      double b= s.nextDouble();
+	      System.out.println("Enter the height of the Triangle:");
+	      double h= s.nextDouble();
 	      double area=(b*h)/2;
 	      System.out.println("Area of Triangle is: " + area);      
 	   }
