14740cc6d784419fb4907094adffadd8a154b06e
sivasamyd <88651955+sivasamyd@users.noreply.github.com>
2021-09-21 14:19:59 +0530

    Add files via upload

---
+++ Assignment_1/EvenNumbers.java
@@ -0,0 +1,19 @@
+package program1;
+
+public class EvenNumbers {
+		public static void main(String args[])   
+	{  
+	int number=50;  
+	System.out.print("List of even numbers from 1 to "+number+": ");  
+	for (int i=1; i<=number; i++)   
+	{  
+	//logic to check if the number is even or not  
+	//if i%2 is equal to zero, the number is even  
+	if (i%2==0)   
+	{  
+	System.out.print(i + " ");  
+	}  
+	}  
+	}  
+	
+}
