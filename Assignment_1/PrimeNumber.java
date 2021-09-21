e0a96e8fa60cd672606a09d6d0ae2c901438e1ff
sivasamyd <88651955+sivasamyd@users.noreply.github.com>
2021-09-21 14:17:23 +0530

    Add files via upload

---
+++ Assignment_1/PrimeNumber.java
@@ -0,0 +1,32 @@
+package program1;
+
+public class PrimeNumber {  
+	 public static void main (String[] args)
+	   {		
+	       int i =0;
+	       int num =0;
+	       //Empty String
+	       String  primeNumbers = "";
+
+	       for (i = 1; i <= 30; i++)         
+	       { 		  	  
+	          int counter=0; 	  
+	          for(num =i; num>=1; num--)
+		  {
+	             if(i%num==0)
+		     {
+	 		counter = counter + 1;
+		     }
+		  }
+		  if (counter ==2)
+		  {
+		     //Appended the Prime number to the String
+		     primeNumbers = primeNumbers + i + " ";
+		  }	
+	       }	
+	       System.out.println("Prime numbers from 1 to 10 are :");
+	       System.out.println(primeNumbers);
+	   }
+	} 
+		
+
