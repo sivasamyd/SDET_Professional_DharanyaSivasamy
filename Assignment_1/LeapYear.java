d65226f8571a66c2e1620c617f687f4408c46fcf
sivasamyd <88651955+sivasamyd@users.noreply.github.com>
2021-09-21 14:31:18 +0530

    Update LeapYear.java

--- Assignment_1/LeapYear.java
+++ Assignment_1/LeapYear.java
@@ -10,9 +10,9 @@ public class LeapYear {
 		      year = sc.nextInt();
 
 		      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-		         System.out.println("Specified year is a leap year");
+		         System.out.println("Its a leap year");
 		      else
-		         System.out.println("Specified year is not a leap year");
+		         System.out.println("Its not a leap year");
 		   }
 		
 }
