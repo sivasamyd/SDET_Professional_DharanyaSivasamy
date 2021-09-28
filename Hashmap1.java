9b60020a0af2b5760740c88591eff93c44e5a150
sivasamyd <88651955+sivasamyd@users.noreply.github.com>
2021-09-28 17:17:09 +0530

    Add files via upload

---
+++ Hashmap1.java
@@ -0,0 +1,41 @@
+package program4;
+import java.util.*;
+public class Hashmap1 {
+	    public static void
+	    countDuplicateCharacters(String str)
+	    {
+	  
+	        Map<Character, Integer> map
+	            = new HashMap<Character, Integer>();
+
+	        char[] charArray = str.toCharArray();
+
+	        for (char c : charArray) {
+	  
+	            if (map.containsKey(c)) {
+	                map.put(c, map.get(c) + 1);
+	            }
+	            else {
+
+	                map.put(c, 1);
+	            }
+	        }
+	  
+	        for (Map.Entry<Character, Integer> entry :
+	             map.entrySet()) {
+	  
+	            if (entry.getValue() > 1) {
+	                System.out.println(entry.getKey()
+	                                   + " : "
+	                                   + entry.getValue());
+	            }
+	        }
+	    }
+
+	    public static void
+	    main(String args[])
+	    {
+	        String str = "devlabsalliancetraining";
+	        countDuplicateCharacters(str);
+	    }
+	}
